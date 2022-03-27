06d1a49b56ee5ece55bdafeefd57e6af55cf58a7
alinivan <ai626@live.mdx.ac.uk>
2022-03-27 22:01:01 +0100

    added more menu options

--- main.py
+++ main.py
@@ -6,23 +6,31 @@ from data import getData
 from menu import getMenu
 
 #init
-CATEGORIES = ["carton", "plastic"]
+CATEGORIES = ("carton", "plastic")
 data = getData(CATEGORIES, [100, 100])
     
 #start
 def start():
     print("")
     opt = getMenu()
-    print("Response:")
+    print("response:")
     
     if opt == "1":
-        print(len(data))
+        print("data size: " + str(len(data)))
+        for cat in data:
+            print(cat + " size: " + str(len(data[cat])))
         
     elif opt == "2":
-        print(plt.imshow(data["plastic"][7]/255))  
+        for cat in data:
+            for i in range(0, 12):
+                print(plt.imshow(data[cat][i]/255))
+                plt.pause(0.001)
         
     elif opt == "3":
         print(data["plastic"][0].shape)
+        
+    elif opt == "4":
+        print(data['plastic'][0])
         
     elif opt == "0":
         sys.exit("Terminated")
--- menu.py
+++ menu.py
@@ -1,9 +1,10 @@
 #menu script
 
 def showOptions():
-    print("[1] Print array size")
-    print("[2] Display single image")
+    print("[1] Print data size")
+    print("[2] Display images")
     print("[3] Print image shape")
+    print("[4] Print image (np array)")
     print("[0] Exit")
 
 def getMenu():
